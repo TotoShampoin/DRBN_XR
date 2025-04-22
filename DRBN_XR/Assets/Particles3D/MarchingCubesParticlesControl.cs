dd38535e5b1d2bb8a97aa14aaf1e37431c28c7e6
TotoShampoin <thomas.garnier@totoshampoin.fr>
2025-04-22 16:17:29 +0200

    marching the particules

---
+++ DRBN_XR/Assets/Particles3D/MarchingCubesParticlesControl.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MarchingCubesParticlesControl : MonoBehaviour
+{
+    [SerializeField] WeightGenerator weightGenerator;
+    [SerializeField] RenderTexture renderTexture;
+    [SerializeField] MarchingCubesRef marchingCubes;
+
+    void OnEnable()
+    {
+        Regenerate();
+    }
+
+    void Update()
+    {
+        if (!renderTexture) return;
+        weightGenerator.Generate(renderTexture);
+        marchingCubes
+            .GenerateAndApplyMesh(renderTexture, weightGenerator.Threshold);
+    }
+
+    void Regenerate()
+    {
+    }
+}
