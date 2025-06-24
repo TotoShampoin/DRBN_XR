c2e5a9329f057e65fd742588b03b246214102892
TotoShampoin <thomas.garnier@totoshampoin.fr>
2025-06-24 15:34:45 +0200

    Added in-vr console

---
+++ DRBN_XR/Assets/DRBN_Membrane/Misc/DebugLogInXR.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+
+public class DebugLogInXR : MonoBehaviour
+{
+    public LayoutGroup console;
+    public TextMeshProUGUI linePrefab;
+    public int maxLines = 20;
+    public bool logs = true;
+    public bool warning = true;
+    public bool errors = true;
+    readonly Queue<TextMeshProUGUI> logQueue = new();
+
+    void Start()
+    {
+        linePrefab.gameObject.SetActive(false);
+    }
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        var logLine = Instantiate(linePrefab, console.transform);
+        if (!logs && type == LogType.Log) return;
+        if (!warning && type == LogType.Warning) return;
+        if (!errors && (type == LogType.Error || type == LogType.Exception || type == LogType.Assert)) return;
+        logLine.color = type switch
+        {
+            LogType.Error => Color.red,
+            LogType.Exception => Color.red,
+            LogType.Assert => Color.red,
+            LogType.Warning => Color.yellow,
+            LogType.Log => Color.white,
+            _ => Color.gray,
+        };
+        logLine.text = logString;
+        logQueue.Enqueue(logLine);
+        logLine.gameObject.SetActive(true);
+        while (logQueue.Count > maxLines)
+        {
+            var oldLine = logQueue.Dequeue();
+            Destroy(oldLine.gameObject);
+        }
+    }
+}
